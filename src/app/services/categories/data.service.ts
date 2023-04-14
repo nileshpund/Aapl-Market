1f4a84f9fb9f21786ce408c0deb0dc27520c4270
ashwetadesai <ashwetadesai3010@gmail.com>
2023-04-14 09:44:40 +0530

    added image array

--- src/app/services/categories/data.service.ts
+++ src/app/services/categories/data.service.ts
@@ -41,4 +41,32 @@ export class DataService {
 
     return categories;
   }
+  getTrendycloths() {
+    let trendycloths = [];
+
+    let cat1: ICategory = {
+      id: 1,
+      name: 'Women',
+      image: '../../assets/depositphoto.jpg'
+    }
+    let cat2: ICategory = {
+      id: 2,
+      name: 'Mens',
+      image: '../../assets/depositphotos_4.jpg'
+    }
+    let cat3: ICategory = {
+      id: 3,
+      name: 'Kids',
+      image: '../../assets/category-3.png'
+    }
+    let cat4: ICategory = {
+      id: 4,
+      name: 'Others',
+      image: '../../assets/light-colored-womens-summer-clothes-shoes-hanger_176873-11430.jpg'
+    }
+
+    trendycloths.push(cat1, cat2, cat3,cat4);
+
+    return trendycloths;
+  }
 }
